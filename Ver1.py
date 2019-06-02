6a642d182781f70e8ce6d3d00d4d23faf8db989b
yibre <cdoyue@gmail.com>
2019-06-02 19:37:51 +0900

    third Commit

--- Ver1.py
+++ Ver1.py
@@ -1,13 +1,19 @@
 import sys
 
+read_access_count = 0
+write_access_count = 0
+total_access_count = 0
+read_miss_count = 0
+write_miss_count = 0
+clean_eviction_count = 0
+dirty_eviction_count = 0
 
-class CacheSet:
+class CacheSet: # 여기는 건드릴 일 없음! 더이상 이 class를 호출할 일도, 함수를 쓸 일도 없음!
     def __init__(self, associativity):
         block = {"valid": False, "dirty": False, "tag": None}
         self.set = []
         for i in range(associativity):
             self.set.append(block)
-
 
 class Cache:
     def __init__(self, associativity, index):
@@ -18,6 +24,24 @@ class Cache:
     def find(self, index, tag):
         self.Cache[index]
         pass
+
+    def add(self, tag):
+        pass
+
+    def read(self, address):
+        pass
+
+    def write(self, address):
+        pass
+
+    def file_read(self, file):
+        for line in file:
+            command, address_str = line.rstrip().split(' ')
+            address = int(address_str, 16)
+            if command == 'R':
+                pass
+            else:
+                pass
 
 
 if __name__ == "__main__":
