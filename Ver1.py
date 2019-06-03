36ef74e983ce46ec195b68d7ecede4080633efab
yibre <cdoyue@gmail.com>
2019-06-03 09:40:23 +0900

    fourth Commit

--- Ver1.py
+++ Ver1.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 read_access_count = 0
 write_access_count = 0
@@ -10,29 +11,46 @@ dirty_eviction_count = 0
 
 class CacheSet: # 여기는 건드릴 일 없음! 더이상 이 class를 호출할 일도, 함수를 쓸 일도 없음!
     def __init__(self, associativity):
-        block = {"valid": False, "dirty": False, "tag": None}
+        block = {"valid": False, "dirty": False, "tag": None} # 한 cache block
         self.set = []
         for i in range(associativity):
             self.set.append(block)
 
 class Cache:
-    def __init__(self, associativity, index):
+    def __init__(self, associativity, blockSize, capacity):
         self.Cache = []
-        for i in range(index):
+        self.blockOffset = math.log2(blockSize) # offset = log_2(blockSize)
+        self.indexLength = capacity / blockSize / associativity * 1024
+        # capacity(KB) / blockSize(B) / associativity =  index 의 길이
+        # index 가 128까지 있으면 인덱스비트 7개 사용
+        self.IndexBitLength = math.log2(self.indexLength)
+        for i in range(self.indexLength):
             self.Cache.append(CacheSet(associativity))
 
-    def find(self, index, tag):
-        self.Cache[index]
-        pass
-
-    def add(self, tag):
-        pass
-
     def read(self, address):
+        global read_access_count, total_access_count, read_miss_count
+        read_access_count += 1
+        total_access_count += 1
+
         pass
 
     def write(self, address):
+        global write_access_count, total_access_count, write_miss_count
+        write_access_count += 1
+        total_access_count += 1
+
         pass
+
+    def eviction(self, address): # LRU 구현 dirty eviction과 clean eviction 구현
+        pass
+
+    def findTagBit(self, address): # return; int 타입
+        TagBit = int(bin(address)[:-(self.blockOffset+self.IndexBitLength)], 2)
+        return TagBit
+
+    def findIndexBit(self, address): # return: int 타입
+        IndexBit = int('0b'+bin(address)[:(-self.blockOffset)][-(self.IndexBitLength):], 2)
+        return IndexBit
 
     def file_read(self, file):
         for line in file:
